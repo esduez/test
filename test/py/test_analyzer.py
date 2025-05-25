5eb8a246c92d0e18d18d5872233f56fbaaae37c6
esduez <60666283+esduez@users.noreply.github.com>
2025-05-26 01:43:23 +0300

    Update test_analyzer.py

--- test/py/test_analyzer.py
+++ test/py/test_analyzer.py
@@ -1,14 +1,16 @@
 import unittest
 import sys
 import os
+
+# Kritik yol ayarı
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from py.analyzer import analyze_package
 
 class TestAnalyzer(unittest.TestCase):
     def test_analysis(self):
         result = analyze_package("flask")
+        self.assertIsInstance(result, dict)
         self.assertIn("dependencies", result)
-        self.assertIsInstance(result["dependencies"], list)
-
+        
 if __name__ == '__main__':
     unittest.main()
