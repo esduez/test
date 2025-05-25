85f6e4fde96ddcc4b2715d92f451633acbbbb1ab
esduez <60666283+esduez@users.noreply.github.com>
2025-05-26 00:54:29 +0300

    Update test_sample.py

--- py/test_sample.py
+++ py/test_sample.py
@@ -1,7 +1,10 @@
 import unittest
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))]
 
-class TestExample(unittest.TestCase):
-    def test_math(self):
+class TestAnalyzer(unittest.TestCase):
+    def test_dummy(self):
         self.assertEqual(1+1, 2)
 
 if __name__ == '__main__':
