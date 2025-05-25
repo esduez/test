37bb36f6526ff0172d0ef90362bbad68f261df5e
esduez <60666283+esduez@users.noreply.github.com>
2025-05-26 00:58:07 +0300

    Update cli.py

--- py/cli.py
+++ py/cli.py
@@ -1,11 +1,16 @@
 import json
 import sys
-from analyzer import analyze
+from analyzer import analyze_package
 
 def main():
-    if len(sys.argv) > 1:
-        result = analyze(sys.argv[1])
-        print(json.dumps(result))
+    try:
+        command = sys.argv[1]
+        if command == "analyze":
+            result = analyze_package(sys.argv[2])
+            print(json.dumps(result))
+    except Exception as e:
+        print(json.dumps({"error": str(e)}))
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
